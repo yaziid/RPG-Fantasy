b8973139c472cb0fe947b28d1e6f414c6ae665e5
yaziid <cantinanv@hotmail.com>
2014-08-17 21:01:44 +0200

    Update Map.hpp

--- Sources/Map.hpp
+++ Sources/Map.hpp
@@ -7,13 +7,14 @@
 class Map
 {
 public :
-
     void load(std::string filename, sf::RenderTarget& target);
 
 private :
 
     sf::Texture mTileTexture;
     sf::Sprite mTiles;
+    int m_tilewidth;
+    int m_tileheight;
 };
 
 #endif // MAP_HPP_INCLUDED
